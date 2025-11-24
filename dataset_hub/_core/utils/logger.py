cae3c3425773c764817b4493c7470373b03fe0bd
Slava0505 <slava38710505@gmail.com>
2025-11-24 23:15:16 +0700

    docs: add docs to logger

--- dataset_hub/_core/utils/logger.py
+++ dataset_hub/_core/utils/logger.py
@@ -5,6 +5,12 @@ from typing import Optional
 
 def get_logger(name: Optional[str] = None) -> Logger:
     """
-    Возвращает логгер для внутреннего использования.
+    Get a logger instance with the specified name.
+
+    Args:
+        name (Optional[str]): The name of the logger. If None, the root logger is returned.
+
+    Returns:
+        Logger: A logging.Logger instance corresponding to the given name.
     """
     return logging.getLogger(name)
