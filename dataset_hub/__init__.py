fb98968d8dd6a7ce62c0323288ab4caf6aa126a7
andeytwen-ctrl <andeytwen@gmail.com>
2026-02-04 16:38:09 +0700

    Update __init__.py

--- dataset_hub/__init__.py
+++ dataset_hub/__init__.py
@@ -1,5 +1,5 @@
-from . import classification, regression, timeseries
+from . import classification, regression, timeseries, nlp
 from ._core.data_bundle import DataBundle
 from ._core.settings.user_settings import set_option
 
-__all__ = ["classification", "regression", "timeseries", "set_option", "DataBundle"]
+__all__ = ["classification", "regression", "timeseries", "set_option", "DataBundle","nlp"]
